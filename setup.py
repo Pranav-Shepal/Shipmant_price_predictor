2792acc9362ce85204d757d96137f804d4e42224
Pranav Shepal <pranavshepal07@gmail.com>
2024-11-22 19:27:26 +0530

    added read me file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="Pranav",
+    author_email="pranavshepal@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
